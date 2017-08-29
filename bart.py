14e9f572cc737205c3a78836774cb184ca45c989
Asmita Vikas <vikasmita20@gmail.com>
2017-08-29 15:39:05 -0700

    code till melt

--- bart.py
+++ bart.py
@@ -7,22 +7,28 @@ import os
 import shutil
 
 
+
+
 def extract_zip(saveDir, mainDir):
     for filename in glob.glob(os.path.join(mainDir, '*.zip')):
-        print filename
+#       print filename
         zip = zipfile.ZipFile(filename)
         zip.extractall(saveDir)
 
 
-def melt_df(rider_data, mon, yr, day_type):
-    print rider_data[0]
-    var_id =  rider_data.index
-    print var_id
-    melted_df = pandas.melt(rider_data, id_vars="RM", value_vars=[], var_name='start',
-                value_name='riders')
-    print melted_df
 
+
+def melt_df(rider_data, mon, yr, day_type):
+
+    rider_data = rider_data.stack().reset_index()
+    rider_data.columns = ['terms', 'start', 'riders']
+    rider_data['year'] = yr
+    rider_data['month'] = mon
+    rider_data['day_type'] = day_type
+    return rider_data
+    
 def read_xls(saveDir, sheets_name):
+    final_df = pandas.DataFrame()
     for root, dirs, files in os.walk(saveDir):
         for file in files:
             book = xlrd.open_workbook(root+'/'+file)
@@ -31,9 +37,9 @@ def read_xls(saveDir, sheets_name):
             for column in range(sheets[0].ncols):
                     if "Exits" == sheets[0].cell(1, column).value:
                         col_end = column-1
-                        print col_end
 
             daytype = sheets[0].cell(0,3).value.lower()
+           
             for word in ['weekday', 'sunday', 'saturday']:
                 if word in daytype:
                     daytype = word
@@ -42,16 +48,18 @@ def read_xls(saveDir, sheets_name):
 
 
             for sheet in sheets_name:
-              rider_df = pandas.read_excel(root+'/'+file, sheet, header=1, skiprows=0, index_col=0,
+                rider_df = pandas.read_excel(root+'/'+file, sheet, header=1, skiprows=0, index_col=0,
                                          skip_footer=1, parse_cols=col_end)
-              melt_df(rider_df, month, year, daytype)
+
+            melted_riders = melt_df(rider_df, month, year, daytype)
+            final_df.append(melted_riders)
+    print final_df    
 
 
 
 
 def ProcessBart(saveDir, mainDir, SQLConn, schema, table):
-
-    # extract_zip(saveDir, mainDir)
+    extract_zip(saveDir, mainDir)
     sheets = ['Wkdy Adj OD', 'Sat Adj OD', 'Sun Adj OD']
     read_xls(saveDir, sheets)
 
@@ -59,5 +67,7 @@ def ProcessBart(saveDir, mainDir, SQLConn, schema, table):
 
 tmpDir = "tmpDIR"
 dataDir = "BART_DATA"
+
+
 
 ProcessBart(tmpDir, dataDir, SQLConn=None, schema="cls", table="bart")
